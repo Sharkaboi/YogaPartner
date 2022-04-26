0ac628b00a8b06123d20e9f2092d6b0808dee919
Sarath S <sarathsedu@gmail.com>
2022-04-26 16:40:11 +0530

    Add permission check

--- android/app/src/main/java/com/sharkaboi/yogapartner/modules/asana_list/ui/AsanaListFragment.kt
+++ android/app/src/main/java/com/sharkaboi/yogapartner/modules/asana_list/ui/AsanaListFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.sharkaboi.yogapartner.NavGraphDirections
+import com.sharkaboi.yogapartner.common.extensions.showToast
 import com.sharkaboi.yogapartner.databinding.FragmentAsanaListBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,6 +17,10 @@ class AsanaListFragment : Fragment() {
     private var _binding: FragmentAsanaListBinding? = null
     private val binding get() = _binding!!
     private val navController get() = findNavController()
+    private val permLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            handlePermissionCheck(it)
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,8 +42,21 @@ class AsanaListFragment : Fragment() {
 
     private fun init() {
         binding.fabOpenPose.setOnClickListener {
-            val direction = NavGraphDirections.openAsanaPose()
-            navController.navigate(direction)
+            checkPermission()
         }
+    }
+
+    private fun checkPermission() {
+        permLauncher.launch(android.Manifest.permission.CAMERA)
+    }
+
+    private fun handlePermissionCheck(wasGranted: Boolean) {
+        if (!wasGranted) {
+            showToast("Need Camera permission")
+            return
+        }
+
+        val direction = NavGraphDirections.openAsanaPose()
+        navController.navigate(direction)
     }
 }
