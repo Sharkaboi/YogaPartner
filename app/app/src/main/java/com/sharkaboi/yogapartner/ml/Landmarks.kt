d0843ac496fde745d35f4f1788699a453ccfe135
Sarath S <sarathsedu@gmail.com>
2022-04-26 16:29:06 +0530

    Remove refactored app

--- app/app/src/main/java/com/sharkaboi/yogapartner/ml/Landmarks.kt
+++
@@ -1,9 +0,0 @@
-package com.sharkaboi.yogapartner.ml
-
-import android.graphics.Bitmap
-import com.google.mlkit.vision.pose.Pose
-
-data class Landmarks(
-    val pose: Pose,
-    val originalImage: Bitmap?
-)
