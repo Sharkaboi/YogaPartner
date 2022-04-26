cbfbb14f3b0c600b534e8d133a656693292e9f15
Sarath S <sarathsedu@gmail.com>
2022-04-26 12:52:51 +0530

    Code refactoring - Remove useless functions

--- android/app/src/main/java/com/sharkaboi/yogapartner/ml/processor/PoseDetectorProcessor.kt
+++ android/app/src/main/java/com/sharkaboi/yogapartner/ml/processor/PoseDetectorProcessor.kt
@@ -1,9 +1,6 @@
-
-
 package com.sharkaboi.yogapartner.ml.processor
 
 import android.content.Context
-import android.util.Log
 import com.google.android.gms.tasks.Task
 import com.google.android.odml.image.MlImage
 import com.google.mlkit.vision.common.InputImage
@@ -12,105 +9,106 @@ import com.google.mlkit.vision.pose.PoseDetection
 import com.google.mlkit.vision.pose.PoseDetector
 import com.google.mlkit.vision.pose.PoseDetectorOptionsBase
 import com.sharkaboi.yogapartner.ml.classification.PoseClassifierProcessor
-import com.sharkaboi.yogapartner.modules.asana_pose.ui.PoseGraphic
 import com.sharkaboi.yogapartner.modules.asana_pose.camera.GraphicOverlay
+import com.sharkaboi.yogapartner.modules.asana_pose.ui.PoseGraphic
 import timber.log.Timber
-import java.util.ArrayList
+import java.util.*
 import java.util.concurrent.Executor
 import java.util.concurrent.Executors
 
 /** A processor to run pose detector. */
 class PoseDetectorProcessor(
-  private val context: Context,
-  options: PoseDetectorOptionsBase,
-  private val showInFrameLikelihood: Boolean,
-  private val visualizeZ: Boolean,
-  private val rescaleZForVisualization: Boolean,
-  private val runClassification: Boolean,
-  private val isStreamMode: Boolean
+    private val context: Context,
+    options: PoseDetectorOptionsBase,
+    private val showInFrameLikelihood: Boolean,
+    private val visualizeZ: Boolean,
+    private val rescaleZForVisualization: Boolean,
+    private val runClassification: Boolean,
+    private val isStreamMode: Boolean
 ) : VisionProcessorBase<PoseDetectorProcessor.PoseWithClassification>(context) {
 
-  private val detector: PoseDetector = PoseDetection.getClient(options)
-  private val classificationExecutor: Executor
+    private val detector: PoseDetector = PoseDetection.getClient(options)
+    private val classificationExecutor: Executor
 
-  private var poseClassifierProcessor: PoseClassifierProcessor? = null
+    private var poseClassifierProcessor: PoseClassifierProcessor? = null
 
-  /** Internal class to hold Pose and classification results. */
-  class PoseWithClassification(val pose: Pose, val classificationResult: List<String>)
+    /** Internal class to hold Pose and classification results. */
+    class PoseWithClassification(val pose: Pose, val classificationResult: List<String>)
 
-  init {
-    classificationExecutor = Executors.newSingleThreadExecutor()
-  }
+    init {
+        classificationExecutor = Executors.newSingleThreadExecutor()
+    }
 
-  override fun stop() {
-    super.stop()
-    detector.close()
-  }
+    override fun stop() {
+        super.stop()
+        detector.close()
+    }
 
-  override fun detectInImage(image: InputImage): Task<PoseWithClassification> {
-    return detector
-      .process(image)
-      .continueWith(
-        classificationExecutor,
-        { task ->
-          val pose = task.result
-          var classificationResult: List<String> = ArrayList()
-          if (runClassification) {
-            if (poseClassifierProcessor == null) {
-              poseClassifierProcessor =
-                PoseClassifierProcessor(
-                  context,
-                  isStreamMode
-                )
-            }
-            classificationResult = poseClassifierProcessor!!.getPoseResult(pose)
-          }
-          PoseWithClassification(pose, classificationResult)
-        }
-      )
-  }
+    // For InputImage type
+    override fun detectInImage(image: InputImage): Task<PoseWithClassification> {
+        return detector
+            .process(image)
+            .continueWith(
+                classificationExecutor,
+                { task ->
+                    val pose = task.result
+                    var classificationResult: List<String> = ArrayList()
+                    if (runClassification) {
+                        if (poseClassifierProcessor == null) {
+                            poseClassifierProcessor =
+                                PoseClassifierProcessor(
+                                    context,
+                                    isStreamMode
+                                )
+                        }
+                        classificationResult = poseClassifierProcessor!!.getPoseResult(pose)
+                    }
+                    PoseWithClassification(pose, classificationResult)
+                }
+            )
+    }
 
-  // do not call super.detectInImage
-  override fun detectInImage(image: MlImage): Task<PoseWithClassification> {
-    return detector
-      .process(image)
-      .continueWith(
-        classificationExecutor,
-        { task ->
-          val pose = task.getResult()
-          var classificationResult: List<String> = ArrayList()
-          if (runClassification) {
-            if (poseClassifierProcessor == null) {
-              poseClassifierProcessor =
-                PoseClassifierProcessor(
-                  context,
-                  isStreamMode
-                )
-            }
-            classificationResult = poseClassifierProcessor!!.getPoseResult(pose)
-          }
-          PoseWithClassification(pose, classificationResult)
-        }
-      )
-  }
+    // For MlImage type
+    override fun detectInImage(image: MlImage): Task<PoseWithClassification> {
+        return detector
+            .process(image)
+            .continueWith(
+                classificationExecutor,
+                { task ->
+                    val pose = task.getResult()
+                    var classificationResult: List<String> = ArrayList()
+                    if (runClassification) {
+                        if (poseClassifierProcessor == null) {
+                            poseClassifierProcessor =
+                                PoseClassifierProcessor(
+                                    context,
+                                    isStreamMode
+                                )
+                        }
+                        classificationResult = poseClassifierProcessor!!.getPoseResult(pose)
+                    }
+                    PoseWithClassification(pose, classificationResult)
+                }
+            )
+    }
 
-  override fun onSuccess(
-    results: PoseWithClassification,
-    graphicOverlay: GraphicOverlay
-  ) {
-    graphicOverlay.add(
-      PoseGraphic(
-        graphicOverlay,
-        results.pose,
-        showInFrameLikelihood,
-        visualizeZ,
-        rescaleZForVisualization,
-        results.classificationResult
-      )
-    )
-  }
+    override fun onSuccess(
+        results: PoseWithClassification,
+        graphicOverlay: GraphicOverlay
+    ) {
+        graphicOverlay.add(
+            PoseGraphic(
+                graphicOverlay,
+                results.pose,
+                showInFrameLikelihood,
+                visualizeZ,
+                rescaleZForVisualization,
+                results.classificationResult
+            )
+        )
+    }
 
-  override fun onFailure(e: Exception) {
-    Timber.d( "Pose detection failed!", e)
-  }
+    override fun onFailure(e: Exception) {
+        Timber.d("Pose detection failed!", e)
+    }
 }
--- android/app/src/main/java/com/sharkaboi/yogapartner/ml/processor/VisionProcessorBase.kt
+++ android/app/src/main/java/com/sharkaboi/yogapartner/ml/processor/VisionProcessorBase.kt
@@ -86,109 +86,6 @@ abstract class VisionProcessorBase<T>(context: Context) : VisionImageProcessor {
         )
     }
 
-    // -----------------Code for processing single still image----------------------------------------
-    override fun processBitmap(bitmap: Bitmap?, graphicOverlay: GraphicOverlay) {
-        val frameStartMs = SystemClock.elapsedRealtime()
-
-        if (isMlImageEnabled(graphicOverlay.context)) {
-            val mlImage = BitmapMlImageBuilder(bitmap!!).build()
-            requestDetectInImage(
-                mlImage,
-                graphicOverlay,
-                /* originalCameraImage= */ null,
-                /* shouldShowFps= */ false,
-                frameStartMs
-            )
-            mlImage.close()
-            return
-        }
-
-        requestDetectInImage(
-            InputImage.fromBitmap(bitmap!!, 0),
-            graphicOverlay,
-            /* originalCameraImage= */ null,
-            /* shouldShowFps= */ false,
-            frameStartMs
-        )
-    }
-
-    // -----------------Code for processing live preview frame from Camera1 API-----------------------
-    @Synchronized
-    override fun processByteBuffer(
-        data: ByteBuffer?,
-        frameMetadata: FrameMetadata?,
-        graphicOverlay: GraphicOverlay
-    ) {
-        latestImage = data
-        latestImageMetaData = frameMetadata
-        if (processingImage == null && processingMetaData == null) {
-            processLatestImage(graphicOverlay)
-        }
-    }
-
-    @Synchronized
-    private fun processLatestImage(graphicOverlay: GraphicOverlay) {
-        processingImage = latestImage
-        processingMetaData = latestImageMetaData
-        latestImage = null
-        latestImageMetaData = null
-        if (processingImage != null && processingMetaData != null && !isShutdown) {
-            processImage(processingImage!!, processingMetaData!!, graphicOverlay)
-        }
-    }
-
-    private fun processImage(
-        data: ByteBuffer,
-        frameMetadata: FrameMetadata,
-        graphicOverlay: GraphicOverlay
-    ) {
-        val frameStartMs = SystemClock.elapsedRealtime()
-        // If live viewport is on (that is the underneath surface view takes care of the camera preview
-        // drawing), skip the unnecessary bitmap creation that used for the manual preview drawing.
-        val bitmap =
-            if (DetectorOptions.isCameraLiveViewportEnabled()) null
-            else BitmapUtils.getBitmap(data, frameMetadata)
-
-        if (isMlImageEnabled(graphicOverlay.context)) {
-            val mlImage =
-                ByteBufferMlImageBuilder(
-                    data,
-                    frameMetadata.width,
-                    frameMetadata.height,
-                    MlImage.IMAGE_FORMAT_NV21
-                )
-                    .setRotation(frameMetadata.rotation)
-                    .build()
-            requestDetectInImage(
-                mlImage,
-                graphicOverlay,
-                bitmap, /* shouldShowFps= */
-                true,
-                frameStartMs
-            )
-                .addOnSuccessListener(executor) { processLatestImage(graphicOverlay) }
-
-            // This is optional. Java Garbage collection can also close it eventually.
-            mlImage.close()
-            return
-        }
-
-        requestDetectInImage(
-            InputImage.fromByteBuffer(
-                data,
-                frameMetadata.width,
-                frameMetadata.height,
-                frameMetadata.rotation,
-                InputImage.IMAGE_FORMAT_NV21
-            ),
-            graphicOverlay,
-            bitmap,
-            /* shouldShowFps= */ true,
-            frameStartMs
-        )
-            .addOnSuccessListener(executor) { processLatestImage(graphicOverlay) }
-    }
-
     // -----------------Code for processing live preview frame from CameraX API-----------------------
     @ExperimentalGetImage
     override fun processImageProxy(image: ImageProxy, graphicOverlay: GraphicOverlay) {
@@ -236,7 +133,7 @@ abstract class VisionProcessorBase<T>(context: Context) : VisionImageProcessor {
             .addOnCompleteListener { image.close() }
     }
 
-    // -----------------Common processing logic-------------------------------------------------------
+    // For InputImage input
     private fun requestDetectInImage(
         image: InputImage,
         graphicOverlay: GraphicOverlay,
@@ -253,6 +150,7 @@ abstract class VisionProcessorBase<T>(context: Context) : VisionImageProcessor {
         )
     }
 
+    // For MlImage input
     private fun requestDetectInImage(
         image: MlImage,
         graphicOverlay: GraphicOverlay,
@@ -377,14 +275,7 @@ abstract class VisionProcessorBase<T>(context: Context) : VisionImageProcessor {
 
     protected abstract fun detectInImage(image: InputImage): Task<T>
 
-    protected open fun detectInImage(image: MlImage): Task<T> {
-        return Tasks.forException(
-            MlKitException(
-                "MlImage is currently not demonstrated for this feature",
-                MlKitException.INVALID_ARGUMENT
-            )
-        )
-    }
+    protected abstract fun detectInImage(image: MlImage): Task<T>
 
     protected abstract fun onSuccess(results: T, graphicOverlay: GraphicOverlay)
 
