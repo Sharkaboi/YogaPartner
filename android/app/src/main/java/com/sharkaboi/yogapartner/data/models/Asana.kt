293be4ce284e6eda710b4c995bca5b74d74a34ca
Sarath S <sarathsedu@gmail.com>
2022-05-25 12:58:47 +0530

    Remove unused fields

--- android/app/src/main/java/com/sharkaboi/yogapartner/data/models/Asana.kt
+++ android/app/src/main/java/com/sharkaboi/yogapartner/data/models/Asana.kt
@@ -12,21 +12,8 @@ data class Asana(
     val name: String,
     val description: String,
     val difficulty: AsanaDifficulty,
-    val asanaType: AsanaType,
-    val asanaThumbnail: String,
-    val nameSanskrit: String,
-    val nameEn: String
+    val asanaThumbnail: String
 ) : Parcelable {
-    fun getDbValue() = buildMap {
-        put("asana_name", name)
-        put("asana_description", description)
-        put("asana_difficulty", difficulty.getDbValue())
-        put("asana_type", asanaType.getDbValue())
-        put("asana_thumbnail", asanaThumbnail)
-        put("asana_name_sanskrit", nameSanskrit)
-        put("asana_name_en", nameEn)
-    }
-
     companion object {
         private fun getFromDbValue(document: DocumentSnapshot): Asana {
             return Asana(
@@ -34,10 +21,7 @@ data class Asana(
                 name = document["asana_name"].toString(),
                 description = document["asana_description"].toString(),
                 difficulty = AsanaDifficulty.parseFromDb((document["asana_difficulty"] as Long).toInt()),
-                asanaType = AsanaType.parseFromDb(document["asana_type"].toString()),
                 asanaThumbnail = document["asana_thumbnail"].toString(),
-                nameSanskrit = document["asana_name_sanskrit"].toString(),
-                nameEn = document["asana_name_en"].toString()
             )
         }
 
